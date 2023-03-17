ea65360be0310501fb8d01e8d415767cd4b6395c
ylm78 <104013654+ylm78@users.noreply.github.com>
2023-03-17 18:31:32 +0100

    Add files via upload

--- function.py
+++ function.py
@@ -1,20 +1,18 @@
-#Fonction qui vérifie si un état est une entrée ou une sortie
-def es_or_not(val, nb_init, nb_term, init, term):
-    result = []
-    for i in range(0, nb_init):
-        if val == init[i]:
-            result.append("E")
-    for j in range(0, nb_term):
-        if val == term[j]:
-            result.append("S")
-    return result
+import re
 
-def es_or_not(val,nb_init_term,init_term):
-    for i in range(0, nb_init_term):
-        if val == init_term[i]:
-            return 1
-    return 0
+def es_or_not(val, nb_init_term, init_term):
+    if (nb_init_term == 1):
+         return val == init_term
+    if isinstance(init_term, list):
+        return val in init_term
+    init_term_list = [int(x) for x in init_term.split()]
+    return val in init_term_list
 
+def extract_numbers(s):
+    pattern = r'\d+'
+    numbers = re.findall(pattern, s)
+    numbers = [int(n) for n in numbers]
+    return numbers
 
 def get_next_line(f):
     fichier = f.readlines()
@@ -26,3 +24,8 @@ def remove_n(fichier):
         fichier[count] = fichier[count].replace('\n', '')
         count += 1
 
+def add_transition(tab, s):
+    row = int(s[0])
+    col = ord(s[1]) - ord('a') + 2
+    val = s[2]
+    tab[row][col] = val
--- main.py
+++ main.py
@@ -1,20 +1,99 @@
 from function import *
+import pandas as pd
+import string
+import matplotlib.pyplot as plt
 
-f = open('R7-4.txt')
+f = open('file.txt')
 fichier = get_next_line(f)
 remove_n(fichier)
-print(fichier)
 f.close()
 
-auto=[[]]
+fichier[0] = int(fichier[0])
+fichier[1] = int(fichier[1])
+fichier[2] = int(fichier[2])
+if (len(fichier[3]) == 1 ):
+    fichier[3] = int(fichier[3])
+else :
+    fichier[3] = extract_numbers(fichier[3])
+fichier[4] = int(fichier[4])
+if (len(fichier[5]) == 1 ):
+    fichier[5] = int(fichier[5])
+else :
+    fichier[5] = extract_numbers(fichier[5])
 
-for i in range(0, int(fichier[1])):
-     for j in range(0,int(fichier[0]) + 2):
-          if es_or_not(j,fichier[2],fichier[3] and es_or_not(j,fichier[4],fichier[5])):
-               fichier[i].append("ES")
-          elif (es_or_not(j,fichier[2],fichier[3])):
-               fichier[i].append("E")
-          elif (es_or_not(j,fichier[4],fichier[5])):
-               fichier[i].append("S")
-          else:
-               fichier[i] = -1
+
+
+
+auto = [[".."] * (fichier[0] + 2) for i in range(fichier[1])]
+
+for i in range(0, fichier[1]):
+     auto[i][1] = i
+     if es_or_not(i, fichier[2], fichier[3]) and es_or_not(i, fichier[4], fichier[5]):
+         auto[i][0] = "E/S"
+     elif es_or_not(i, fichier[2], fichier[3]):
+         auto[i][0] = "E"
+     elif es_or_not(i, fichier[4], fichier[5]):
+         auto[i][0] = "S"
+     else:
+         auto[i][0] = " "
+
+
+
+for i in range(6, len(fichier)):
+    add_transition(auto, fichier[i])
+
+
+
+df = pd.DataFrame(auto)
+letters = [' ']+ list(string.ascii_lowercase)[:len(df.columns) - 3]
+
+
+df = df.set_axis(['']+['']+letters, axis='columns')
+print(df.to_string(index=False))
+
+import wx
+import wx.grid
+import pandas as pd
+
+
+import wx
+import wx.grid
+import pandas as pd
+
+class MyFrame(wx.Frame):
+    def __init__(self):
+        wx.Frame.__init__(self, None, title="Automate : " + str(fichier[0]) + " états", size=(500, 300))
+        panel = wx.Panel(self)
+        grid = wx.grid.Grid(panel)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+
+        # Importer votre tableau avec Pandas
+        df = pd.DataFrame(auto)
+        # Ajouter les données à la grille
+        grid.CreateGrid(df.shape[0], df.shape[1]-2)
+        # Ajouter les en-têtes de colonnes
+        grid.SetColLabelValue(0, "États")
+        for i in range(0, df.shape[1]):
+            grid.SetColLabelValue(i + 1, string.ascii_lowercase[i])
+        for i in range(0, df.shape[0]):
+            grid.SetRowLabelValue(i, str(auto[i][0]))
+
+
+        for col in range(1, df.shape[1] - 1):
+            for row in range(df.shape[0]):
+                grid.SetCellValue(row, col-1, str(df.iloc[row, col]))
+                # Verrouiller la cellule pour éviter l'édition
+                grid.SetReadOnly(row, col-1)
+
+        # Ajouter la grille au sizer
+        sizer.Add(grid, 1, wx.EXPAND)
+        panel.SetSizer(sizer)
+
+
+app = wx.App()
+frame = MyFrame()
+frame.Show()
+app.MainLoop()
+
+
+
